82fc2ccc073cf7dee5d5d3c7be9eadc900a57cc3
Arda <arda-turkmen@hotmail.com>
2019-03-25 17:59:38 -0500

    Python code to modularize sql initialization and queries

--- sql_functions.py
+++ sql_functions.py
@@ -1,4 +1,4 @@
-
+import csv
 import pandas
 import mysql.connector
 
@@ -8,56 +8,96 @@ def main():
   password = ''
   hostIP = '127.0.0.1'
   dbName = 'galactic'
-  cnx = connect(username,password,hostIP,dbName)
+  cnx = connect(username, password, hostIP, dbName)
   cursor = cnx.cursor()
   pathStr = 'tortoise data.csv'
   tableName = 'tortoise'
-  loadData(pathStr, tableName, cursor)
-  query = "SELECT * FROM "+tableName
+
+
+  with open(pathStr,'rU') as csvfile:
+    cvsreader = csv.reader(csvfile, delimiter=',')
+
+    line_count = 0
+    firstTwo = []
+    for row in cvsreader:
+      if(line_count == 0):
+        firstTwo.append(row)
+      elif(line_count == 1):
+        firstTwo.append(row)
+        createTable(tableName,firstTwo,cursor)
+        loadData(row, tableName, cursor)
+      else:
+        loadData(row, tableName, cursor)
+      line_count += 1
+
+
+  queryTable(tableName,cursor,('School','Day'))
+
+
+  print("Done")
+  cnx.close()
+
+def queryTable(tableName,cursor,cols_wanted=("*")):
+  query = "SELECT "
+  if (cols_wanted == "*"):
+    query += '*'
+  else:
+    for cols in cols_wanted:
+      query += cols + ','
+    query = query[:-1]
+  query += " FROM " + tableName
   try:
     cursor.execute(query)
+    for row in cursor:
+      for el in row:
+        print(el),
+      print()
   except mysql.connector.Error as err:
     print("Failed showing table: {}".format(err))
 
-  print("Done")
 
-
-
-
-  cnx.close()
-
-
-def loadData(pathStr,tableName,cursor):
-  #df = pandas.read_csv(pathStr)
-  #print(df)
+def createTable(tableName,cols,cursor):
   query = "DROP TABLE IF EXISTS " + tableName
   cursor.execute(query)
+  colIndex = 0
+  query = "CREATE TABLE "+tableName+"( "
+  for col in cols[0]:
+    isDigit = cols[1][colIndex].isdigit()
+    if(isDigit):
+      query += col + " INT DEFAULT NULL, "
+    else:
+      try:
+        float(cols[1][colIndex])
+        query += col + " DOUBLE DEFAULT NULL, "
+      except ValueError:
+        query += col + " VARCHAR(25) DEFAULT NULL, "
 
-  query = ("CREATE TABLE tortoise("
-	"School	VARCHAR(20) NOT NULL,"
-    "Day	VARCHAR(15) NOT NULL,"
-    "col1	VARCHAR(15) NOT NULL,"
-    "Weight	VARCHAR(15) NOT NULL,"
-    "Walking	VARCHAR(15) NOT NULL,"
-    "Eating  VARCHAR(15) NOT NULL,"
-	"Movement VARCHAR(15) DEFAULT NULL)"
-)
+    colIndex += 1
+  query = query[:-2]
+  query += " )"
   try:
     cursor.execute(query)
+    print("Created the table " + tableName)
   except mysql.connector.Error as err:
     print("Failed creating table: {}".format(err))
 
-  pathStr = "'" + pathStr + "'"
-  query = "LOAD DATA INFILE  "
-  query += pathStr
-  query += " INTO TABLE " + tableName
-  query += (" FIELDS TERMINATED BY ','"
-            " LINES TERMINATED BY "+"'\\r'"
-            " IGNORE  1 ROWS")
+def loadData(values,tableName,cursor):
+
+  query = "INSERT INTO "+tableName+" VALUES ("
+  for value in values:
+    if(value != ""):
+      if(value.isalpha):
+        query += "'"+value +"'"+ ','
+      else:
+        query += value + ','
+    else:
+      query += "NULL" + ','
+  query = query[:-1]
+  query += ")"
   try:
     cursor.execute(query)
   except mysql.connector.Error as err:
-    pass
+    print("Failed inserting values to table {}".format(err))
 
 
 
@@ -66,6 +106,7 @@ def connect(username,password,hostIP,dbName):
                               host = hostIP,
                               database=dbName)
 
+  print("Connected to sql server at: " + hostIP + "/" + dbName)
   return cnx
 
 if __name__== "__main__":
